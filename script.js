b923a3a30f869bb7a313df1acf6e1f5850510d21
Brijesh yadav <36631079+recscse@users.noreply.github.com>
2020-07-01 01:55:11 +0530

    add the html ,css and Javascript files

---
+++ script.js
@@ -0,0 +1,31 @@
+function getInputValue(){
+    
+    let inputVal = document.getElementById("inputText").value;
+    if(inputVal==1){
+        documnegt.getElementById("01").style.background="green";
+    }
+    if(inputVal==2){
+        document.getElementById("02").style.background="red";
+    }
+    if(inputVal==3){
+        document.getElementById("03").style.background="blue";
+    }
+    if(inputVal==4){
+        document.getElementById("04").style.background="orange";
+    }
+    if(inputVal==5){
+        document.getElementById("05").style.background="skyblue";
+    }
+    if(inputVal==6){
+        document.getElementById("06").style.background="red";
+    }
+    if(inputVal==7){
+        document.getElementById("07").style.background="pink";
+    }
+    if(inputVal==8){
+        document.getElementById("08").style.background="#325ea8";
+    }
+    if(inputVal==9){
+        document.getElementById("09").style.background="purple";
+    }
+}

5f51f56316cb9594360b1f4af7a36193a225fadf
Brijesh yadav <36631079+recscse@users.noreply.github.com>
2020-07-07 00:30:43 +0530

    Update script.js
    add reset functionality

--- script.js
+++ script.js
@@ -3,29 +3,38 @@ function getInputValue(){
     let inputVal = document.getElementById("inputText").value;
     if(inputVal==1){
        document.getElementById("01").style.background="green";
+        document.getElementById("inputText").value='';
     }
     if(inputVal==2){
         document.getElementById("02").style.background="red";
+        document.getElementById("inputText").value='';
     }
     if(inputVal==3){
         document.getElementById("03").style.background="blue";
+        document.getElementById("inputText").value='';
     }
     if(inputVal==4){
         document.getElementById("04").style.background="orange";
+        document.getElementById("inputText").value='';
     }
     if(inputVal==5){
         document.getElementById("05").style.background="skyblue";
+        document.getElementById("inputText").value='';
     }
     if(inputVal==6){
         document.getElementById("06").style.background="red";
+        document.getElementById("inputText").value='';
     }
     if(inputVal==7){
         document.getElementById("07").style.background="pink";
+        document.getElementById("inputText").value='';
     }
     if(inputVal==8){
         document.getElementById("08").style.background="#325ea8";
+        document.getElementById("inputText").value='';
     }
     if(inputVal==9){
         document.getElementById("09").style.background="purple";
+        document.getElementById("inputText").value='';
     }
 }
